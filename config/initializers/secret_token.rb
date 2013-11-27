5b77daa62b782ce6658c182892daa702fd30f715
Figaeus <figaeus@gmx.com>
2013-11-27 22:25:52 +0530

    Generate Rails 4 application skeleton
    Skipped:
    - ActiveRecord
    - Test::Unit
    - Javascript files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RagPicker::Application.config.secret_key_base = '542e7ab736420f3dda09a77d59bedcecd49c7b7f8e67078342d5e1ebb7dc2a84e930c0e9366115b2c3defb18fa423fd20bce1ab78346d369b0696e2a331ae24d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RagPicker::Application.config.session_store :cookie_store, key: '_rag_picker_session'
