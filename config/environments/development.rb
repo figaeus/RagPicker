acd34c49654bb60764d979e585bee5e96ab5ee23
Figaeus <figaeus@gmx.com>
2013-11-28 23:33:24 +0530

    Use SASS syntax as default rather than SCSS syntax.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,5 @@ RagPicker::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.sass.preferred_syntax = :sass
 end
