35fd830cd4eecb3658998cb483707d0813a66757
Figaeus <figaeus@gmx.com>
2013-11-28 23:53:18 +0530

    Add tests for HomeController#index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe HomeController do
 
+  describe 'GET #index' do
+
+    it 'responds successfully with 200 status code' do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to be 200
+    end
+
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template 'index'
+    end
+
+  end
+
 end
