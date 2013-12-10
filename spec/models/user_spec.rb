fb2efc581d45bfe85139df4785d9e903addc240a
Figaeus <figaeus@gmx.com>
2013-12-10 19:38:09 +0530

    Add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,24 @@ describe User do
   it "fails validations when email is empty" do
     u = User.new
     expect(u.valid?).to be false
+    expect(u.errors[:email]).to include "can't be blank"
+  end
+
+  it "fails validation when email is not valid" do
+    expect do
+      Fabricate :user, email: "xyz@xxx"
+    end.to raise_error Mongoid::Errors::Validations, /Email is invalid/
+  end
+
+  it "allows correctly formatted email addresses" do
+    expect(Fabricate(:user).valid?).to be true
   end
 
   it "fails validation when email is not unique" do
     Fabricate :user
-    expect { Fabricate :user }.to raise_error Mongoid::Errors::Validations
+    expect do
+      Fabricate :user
+    end.to raise_error Mongoid::Errors::Validations, /Email is already taken/
   end
 
 end
