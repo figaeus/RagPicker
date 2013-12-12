80ac79df184060939fc1288a30840ee52f74a3e8
Figaeus <figaeus@gmx.com>
2013-12-12 15:23:16 +0530

    Add specs for bookmark

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -2,23 +2,32 @@ require 'spec_helper'
 
 describe Bookmark do
 
-  it "should belong to creator" do
+  it "has field url of type String" do
+    field = Bookmark.fields['url']
+    expect(field).to be_truthy
+    expect(field.options[:type]).to be String
+  end
+
+  it "has field visits of type Integer" do
+    field = Bookmark.fields['visits']
+    expect(field).to be_truthy
+    expect(field.options[:type]).to be Integer
+  end
+
+  it "belongs to creator" do
     r = Bookmark.reflect_on_association :creator
     expect(r[:relation]).to be Mongoid::Relations::Referenced::In
     expect(r[:class_name]).to eq "User"
     expect(r[:inverse_of]).to be :created_bookmarks
   end
 
-  it "should have field url of type String" do
-    field = Bookmark.fields['url']
-    expect(field).to be_truthy
-    expect(field.options[:type]).to be String
+  it "has many annotations" do
+    r = Bookmark.reflect_on_association :annotations
+    expect(r[:relation]).to be Mongoid::Relations::Referenced::Many
   end
 
-  it "should have field visits of type Integer" do
-    field = Bookmark.fields['visits']
-    expect(field).to be_truthy
-    expect(field.options[:type]).to be Integer
+  it "has many collaborators" do
+    r = Bookmark.reflect_on_association :collaborators
+    expect(r[:relation]).to be Mongoid::Relations::Referenced::Many
   end
-
 end
